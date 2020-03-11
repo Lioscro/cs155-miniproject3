091798bd05337cd33fc6b303bebc27ede9d81b2b
Lioscro <kmin@caltech.edu>
2020-03-10 20:53:41 -0700

    add function to generate

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+import numpy as np
 from keras.layers import Activation, Dense, Lambda, LSTM
 from keras.models import Sequential
 
@@ -15,3 +16,22 @@ class CharacterLSTM(Sequential):
         self.add(Dense(n_chars))
         self.add(Lambda(lambda x: x / temperature))
         self.add(Activation('softmax'))
+
+    def generate(self, seed):
+        X = np.zeros((len(seed), n_chars))
+        indices = np.vstack((np.arange(len(seed)), [char_to_dim[char] for char in seed]))
+        X[tuple(indices)] = 1
+        generated = seed
+
+        # Loop until we have 14 lines.
+        while generated.count('\n') < 14:
+            probabilities = self.predict(np.expand_dims(X, 0)).flatten()
+            #choice = np.argmax(probabilities)
+            choice = np.random.choice(np.arange(n_chars), p=probabilities)
+            generated += dim_to_char[choice]
+
+            # "Push back" X by one and add new prediction to the end.
+            X[:X.shape[0]-1] = X[1:]
+            X[-1] = 0
+            X[-1][choice] = 1
+        return generated
